c50da0d1b6c593330446053d47f90fddc33d1728
Tim Baumann <tim@timbaumann.info>
2014-12-30 23:32:03 +0100

    Implementation of board

--- src/Game/Halma/Board.hs
+++ src/Game/Halma/Board.hs
@@ -2,11 +2,20 @@
 {-# LANGUAGE TypeFamilies #-}
 
 module Game.Halma.Board
-  ( HalmaBoard (..)
+  ( HalmaGrid (..)
   , northCorner, southCorner
   , northeastCorner, northwestCorner
   , southeastCorner, southwestCorner
   , corners
+  , Team (..)
+  , oppositeTeam
+  , startCorner, endCorner
+  , startFields, endFields
+  , twoPlayers, threePlayers
+  , HalmaBoard, toMap, fromMap
+  , lookupHalmaBoard
+  , movePiece
+  , initialBoard
   ) where
 
 import GHC.Generics (Generic)
@@ -15,18 +24,19 @@ import Math.Geometry.Grid.Hexagonal
 import Math.Geometry.Grid.HexagonalInternal (HexDirection (..), UnboundedHexGrid (..))
 import Math.Geometry.GridInternal
 import Data.Maybe (fromJust)
+import qualified Data.Map.Strict as M
 
-data HalmaBoard = HalmaBoard
+data HalmaGrid = HalmaGrid
   deriving (Eq, Show, Read, Ord, Bounded, Generic)
 
 corner :: (HexDirection, HexDirection) -> (Int, Int)
 corner d = iter4 (intoDirection d) center
-  where neighbour' d = fromJust . flip (neighbour UnboundedHexGrid) d
+  where neighbour' dir = fromJust . flip (neighbour UnboundedHexGrid) dir
         intoDirection (d1, d2) = neighbour' d1 . neighbour' d2
         iter4 f = f . f . f . f
         center = (0, 0)
 
-northCorner, southCorner, northeastCorner, northwestCorner, southeastCorner, southwestCorner :: Index HalmaBoard
+northCorner, southCorner, northeastCorner, northwestCorner, southeastCorner, southwestCorner :: Index HalmaGrid
 northCorner = corner (Northwest, Northeast)
 southCorner = corner (Southwest, Southeast)
 northeastCorner = corner (Northeast, East)
@@ -34,17 +44,17 @@ northwestCorner = corner (Northwest, West)
 southeastCorner = corner (Southeast, East)
 southwestCorner = corner (Southwest, West)
 
-corners :: [Index HalmaBoard]
+corners :: [(Int, Int)]
 corners = 
   [ northCorner, southCorner
   , northeastCorner, northwestCorner
   , southeastCorner, southwestCorner
   ]
 
-instance Grid HalmaBoard where
-  type Index HalmaBoard = (Int, Int)
-  type Direction HalmaBoard = HexDirection
-  indices _ = filter (contains HalmaBoard) roughBoard
+instance Grid HalmaGrid where
+  type Index HalmaGrid = (Int, Int)
+  type Direction HalmaGrid = HexDirection
+  indices _ = filter (contains HalmaGrid) roughBoard
     where roughBoard = indices (hexHexGrid 9)
   neighbours = neighboursBasedOn UnboundedHexGrid
   distance = distanceBasedOn UnboundedHexGrid
@@ -55,10 +65,91 @@ instance Grid HalmaBoard where
     where dist = distance UnboundedHexGrid
           distCenter = dist (0, 0) p
 
-instance FiniteGrid HalmaBoard where
-  type Size HalmaBoard = ()
+instance FiniteGrid HalmaGrid where
+  type Size HalmaGrid = ()
   size _ = ()
   maxPossibleDistance _ = 16
 
-instance BoundedGrid HalmaBoard where
+instance BoundedGrid HalmaGrid where
   tileSideCount _ = 6
+
+
+data Team = TeamNorth | TeamSouth
+          | TeamNortheast | TeamNorthwest
+          | TeamSoutheast | TeamSouthwest
+          deriving (Eq, Show, Read, Ord, Bounded, Enum, Generic)
+
+oppositeTeam :: Team -> Team
+oppositeTeam TeamNorth = TeamSouth
+oppositeTeam TeamSouth = TeamNorth
+oppositeTeam TeamNortheast = TeamSouthwest
+oppositeTeam TeamSouthwest = TeamNortheast
+oppositeTeam TeamNorthwest = TeamSoutheast
+oppositeTeam TeamSoutheast = TeamNorthwest
+
+startCorner :: Team -> (Int, Int)
+startCorner TeamNorth = northCorner
+startCorner TeamSouth = southCorner
+startCorner TeamNortheast = northeastCorner
+startCorner TeamNorthwest = northwestCorner
+startCorner TeamSoutheast = southeastCorner
+startCorner TeamSouthwest = southwestCorner
+
+endCorner :: Team -> (Int, Int)
+endCorner = startCorner . oppositeTeam
+
+startFields, endFields :: Team -> [(Int, Int)]
+startFields team = filter ((<= 4) . dist) (indices HalmaGrid)
+  where dist = distance HalmaGrid (startCorner team)
+endFields = startFields . oppositeTeam
+
+twoPlayers :: Team -> Bool
+twoPlayers TeamNorth = True
+twoPlayers TeamSouth = True
+twoPlayers _ = False
+
+threePlayers :: Team -> Bool
+threePlayers TeamNorthwest = True
+threePlayers TeamNortheast = True
+threePlayers TeamSouth = True
+threePlayers _ = False
+
+
+newtype HalmaBoard = HalmaBoard { toMap :: M.Map (Index HalmaGrid) Team }
+  deriving (Eq)
+
+instance Show HalmaBoard where
+  show (HalmaBoard m) = "fromMap (" ++ show m ++ ")"
+
+lookupHalmaBoard :: (Int, Int) -> HalmaBoard -> Maybe Team
+lookupHalmaBoard p = M.lookup p . toMap
+
+fromMap :: M.Map (Index HalmaGrid) Team -> Maybe HalmaBoard
+fromMap m = if invariantsHold then Just (HalmaBoard m) else Nothing
+  where invariantsHold = indicesCorrect && rightTeamPieces
+        list = M.toList m
+        indicesCorrect = all (contains HalmaGrid . fst) list
+        rightTeamPieces = all rightNumberOfTeamPieces [minBound..maxBound]
+        rightNumberOfTeamPieces team =
+          length (filter ((== team) . snd) list) `elem` [0,15]
+
+-- | Move a piece on the halma board. This function does not check whether the
+-- move is valid according to the Halma rules.
+movePiece
+  :: (Int, Int) -- ^ start position
+  -> (Int, Int) -- ^ end position
+  -> HalmaBoard
+  -> Either String HalmaBoard
+movePiece startPos endPos (HalmaBoard m) =
+  case M.lookup startPos m of
+    Nothing -> Left "cannot make move: start position is empty"
+    Just team ->
+      case M.lookup endPos m of
+        Just team' -> Left $ "cannot make move: end position is occupied by team " ++ show team'
+        Nothing -> Right $ HalmaBoard $ M.insert endPos team $ M.delete startPos m
+
+initialBoard :: (Team -> Bool) -> HalmaBoard
+initialBoard chosenTeams = HalmaBoard (M.fromList lineUps)
+  where lineUps = concatMap (\team -> if chosenTeams team then lineUp team else [])
+                            [minBound..maxBound]
+        lineUp team = map (flip (,) team) (startFields team)
--- test/Game/Halma/Board/Tests.hs
+++ test/Game/Halma/Board/Tests.hs
@@ -1,9 +1,18 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# LANGUAGE BangPatterns #-}
+
 module Game.Halma.Board.Tests (tests) where
 
 import Test.HUnit hiding (Test)
+import Test.QuickCheck hiding (Result)
 import Test.Framework
 import Test.Framework.Providers.HUnit (testCase)
-import Data.List (permutations)
+import Test.Framework.Providers.QuickCheck2 (testProperty)
+
+import Data.List (permutations, sortBy)
+import Data.Maybe (isJust, fromJust)
+import Data.Function (on)
+import qualified Data.Map.Strict as M
 
 import Math.Geometry.Grid
 import Math.Geometry.Grid.HexagonalInternal (HexDirection (..))
@@ -12,31 +21,82 @@ import Game.Halma.Board
 testDistancesFromCenter :: Assertion
 testDistancesFromCenter = expected @=? actual
   where center = (0, 0)
-        distCenter = distance HalmaBoard center
+        distCenter = distance HalmaGrid center
         expected = [1, 6, 12, 18, 24, 24, 18, 12, 6, 0, 0, 0]
-        fieldsAtDistance d = filter ((== d) . distCenter) (indices HalmaBoard)
+        fieldsAtDistance d = filter ((== d) . distCenter) (indices HalmaGrid)
         actual = take (length expected) $ map (length . fieldsAtDistance) [0,1..]
 
 testBoundaryLength :: Assertion
 testBoundaryLength = 48 @=? actual
-  where actual = length (boundary HalmaBoard)
+  where actual = length (boundary HalmaGrid)
 
 testDirectionTo :: Assertion
 testDirectionTo = do
-  let center = (0, 0)
-  [Northeast] @=? directionTo HalmaBoard southCorner southeastCorner
-  [Northeast] @=? directionTo HalmaBoard southCorner northeastCorner
+  [Northeast] @=? directionTo HalmaGrid southCorner southeastCorner
+  [Northeast] @=? directionTo HalmaGrid southCorner northeastCorner
   assertOneOf
-    (directionTo HalmaBoard southCorner northCorner)
+    (directionTo HalmaGrid southCorner northCorner)
     (permutations [Northeast, Northwest])
 
 assertOneOf :: (Eq a, Show a) => a -> [a] -> Assertion
 assertOneOf actual validResults = assertBool msg (actual `elem` validResults)
   where msg = "Expected '" ++ show actual ++ "' to be one of '" ++ show validResults ++ "'"
 
+numbersCorrect :: [Maybe Team] -> Bool
+numbersCorrect = go 15 15 (121 - 2*15)
+  where go :: Int -> Int -> Int -> [Maybe Team] -> Bool
+        go 0 0 0 [] = True
+        go !n !s !e (Just TeamNorth : rs) = go (n-1) s e rs
+        go !n !s !e (Just TeamSouth : rs) = go n (s-1) e rs
+        go !n !s !e (Nothing : rs) = go n s (e-1) rs
+        go _ _ _ _ = False
+
+testInitialBoard :: Assertion
+testInitialBoard = assertBool "Expected 15 pieces of team north and south" (numbersCorrect pieces)
+  where pieces = map (\p -> lookupHalmaBoard p (initialBoard twoPlayers)) (indices HalmaGrid)
+
+arbitraryPerm :: [a] -> Gen [a]
+arbitraryPerm xs =
+  fmap (map fst . sortBy (compare `on` snd) . zip xs)
+       (vectorOf (length xs) arbitrary :: Gen [Double])
+
+instance Arbitrary HalmaBoard where
+  arbitrary = do
+    pieces <- arbitraryPerm (indices HalmaGrid)
+    let (northTeam, restPieces) = splitAt 15 pieces
+        southTeam = take 15 restPieces
+    return $ fromJust $ fromMap $ M.fromList $
+      map (flip (,) TeamNorth) northTeam ++
+      map (flip (,) TeamSouth) southTeam
+
+genHalmaGridPos :: Gen (Int, Int)
+genHalmaGridPos = elements (indices HalmaGrid)
+
+prop_fromMap :: HalmaBoard -> Bool
+prop_fromMap halmaBoard = fromMap (toMap halmaBoard) == Just halmaBoard
+
+prop_moveNumbersInvariant :: HalmaBoard -> Gen Bool
+prop_moveNumbersInvariant halmaBoard = do
+  startPos <- genHalmaGridPos
+  endPos <- genHalmaGridPos
+  case movePiece startPos endPos halmaBoard of
+    Left _err ->
+      -- may only fail when there is no piece on the start position or a piece
+      -- on the end position
+      return $ lookupHalmaBoard startPos halmaBoard == Nothing || isJust (lookupHalmaBoard endPos halmaBoard)
+    Right halmaBoard' -> do
+      let pieces = map (\p -> lookupHalmaBoard p (initialBoard twoPlayers)) (indices HalmaGrid)
+      return $ lookupHalmaBoard endPos halmaBoard' == lookupHalmaBoard startPos halmaBoard &&
+               lookupHalmaBoard endPos halmaBoard  == lookupHalmaBoard startPos halmaBoard' &&
+               numbersCorrect pieces
+
+
 tests :: Test
 tests = testGroup "Game.Halma.Board.Tests"
   [ testCase "testDistancesFromCenter" testDistancesFromCenter
   , testCase "testBoundaryLength" testBoundaryLength
   , testCase "testDirectionTo" testDirectionTo
+  , testCase "testInitialBoard" testInitialBoard
+  , testProperty "fromMap" prop_fromMap
+  , testProperty "prop_moveNumbersInvariant" prop_moveNumbersInvariant
   ]
