f375e6abafe776c689d810d0fdcbfec296727d0f
Tim Baumann <tim@timbaumann.info>
2015-01-02 15:38:56 +0100

    Click detection

--- halma.hs
+++ halma.hs
@@ -3,7 +3,9 @@ module Main (main) where
 import Game.Halma.Board
 import Game.Halma.Board.Draw
 import Graphics.UI.Gtk
+import Diagrams.Prelude
 import Diagrams.Backend.Gtk
+import Data.AffineSpace.Point
 import Control.Monad.Trans (liftIO)
 
 main :: IO ()
@@ -11,22 +13,25 @@ main = do
   _ <- initGUI
   window <- windowNew
   canvas <- drawingAreaNew
+  let renderFigure = do
+        --liftIO $ defaultRender canvas (toGtkCoords figure)
+        win <- eventWindow
+        liftIO $ renderToGtk win figure
+        return True
   --button <- buttonNew
-  _ <- canvas `on` sizeRequest $ return (Requisition 256 256)
+  _ <- canvas `on` sizeRequest $ return (Requisition 400 400)
   set window [ containerBorderWidth := 10, containerChild := canvas ]
-  _ <- canvas `on` exposeEvent $ renderFigure canvas
+  _ <- canvas `on` exposeEvent $ renderFigure
   --set button [ buttonLabel := "Hello World" ]
   --onClicked button (putStrLn "Hello World")
   _ <- onDestroy window mainQuit
+  canvas `on` buttonPressEvent $ tryEvent $ do
+    click <- eventClick
+    (x,y) <- eventCoordinates
+    let result = runQuery (query $ toGtkCoords figure) (P (r2 (x, y)))
+    liftIO $ print result
   widgetShowAll window
   mainGUI
-
-renderFigure :: DrawingArea -> EventM EExpose Bool
-renderFigure canvas = do
-  let board  = initialBoard SmallGrid twoPlayers
-      figure = drawBoard board defaultTeamColours
-  --win <- eventWindow
-  --liftIO $ renderToGtk win $ toGtkCoords figure
-  liftIO $ defaultRender canvas figure
-  return True
-
+  where
+    board  = initialBoard SmallGrid twoPlayers
+    figure = toGtkCoords $ scale 20 $ drawBoard board defaultTeamColours
