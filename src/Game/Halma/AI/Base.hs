f26300f916b30c012740e1dd90a64321f4816faf
Matthias Hutzler <matthias.ralph.hutzler@student.uni-augsburg.de>
2015-04-08 13:38:23 +0200

    competitive ai added.

--- src/Game/Halma/AI/Base.hs
+++ src/Game/Halma/AI/Base.hs
@@ -1,13 +1,15 @@
 module Game.Halma.AI.Base
   ( Move
   , outcome
-  , Weighting(..)
+  , Rating(..)
+  , teamRating
   , allOwnPieces
   , allLegalMoves
   ) where
 
 import Math.Geometry.Grid
 import qualified Data.Map.Strict as M
+import Data.List (sort)
 import Game.Halma.Board
 import Game.Halma.Rules
 
@@ -20,24 +22,33 @@ outcome board (source, destination) =
     Left err -> error $ "cannot compute outcome: " ++ err
     Right board' -> board'
 
-data Weighting = WinIn Int
-               | Weighting Float
-               | LossIn Int
+data Rating = WinIn Int
+            | Rating Float
+            | LossIn Int
   deriving (Show, Eq)
 
-instance Ord Weighting where
+instance Ord Rating where
   compare (WinIn n) (WinIn m) = compare m n
   compare (WinIn _) _ = GT
   compare _ (WinIn _) = LT
   compare (LossIn n) (LossIn m) = compare n m
   compare (LossIn _) _ = LT
   compare _ (LossIn _) = GT
-  compare (Weighting a) (Weighting b) = compare a b
+  compare (Rating a) (Rating b) = compare a b
 
-instance Bounded Weighting where
+instance Bounded Rating where
   maxBound = WinIn 0
   minBound = LossIn 0
 
+
+teamRating :: Team -> HalmaBoard size -> Rating
+teamRating team board  | hasFinished board team  = WinIn 0
+                       | otherwise  = Rating . negate . sum $
+                           zipWith (*) pieceWeightingList (sort distances)
+  where distances = map (fromIntegral . distance grid (endCorner grid team)) $
+                        allOwnPieces board team
+        grid = getGrid board
+        pieceWeightingList = replicate 12 2 ++ [3..5]
 
 allOwnPieces :: HalmaBoard size -> Team -> [Index (HalmaGrid size)]
 allOwnPieces board team = 
--- src/Game/Halma/AI/Ignorant.hs
+++ src/Game/Halma/AI/Ignorant.hs
@@ -2,37 +2,27 @@ module Game.Halma.AI.Ignorant
   ( aiMove
   ) where
 
-import Math.Geometry.Grid hiding (null)
-import Data.List (maximumBy, sort)
+import Data.List (maximumBy)
 import Data.Ord (comparing)
 import Game.Halma.Board
 import Game.Halma.Rules
 import Game.Halma.AI.Base
 
 
-weighting :: Team -> HalmaBoard size -> Weighting
-weighting team board  | hasFinished board team  = WinIn 0
-                      | otherwise  = Weighting . negate . sum $
-                          zipWith (*) pieceWeightingList (sort distances)
-  where grid = getGrid board
-        pieceWeightingList = replicate 12 2 ++ [3..5]
-        distances = map (fromIntegral . distance grid (endCorner grid team)) $
-                  allOwnPieces board team
-
-ignorantFixedDepth :: Int -> RuleOptions -> Team -> HalmaBoard size -> Weighting
+ignorantFixedDepth :: Int -> RuleOptions -> Team -> HalmaBoard size -> Rating
 ignorantFixedDepth n opts team board
-  | n<=0  = weighting team board
+  | n<=0  = teamRating team board
   | hasFinished board team  = WinIn 0
-  | otherwise  = beingSomewhatGreedy (weighting team board) $ maximum $
+  | otherwise  = beingSomewhatGreedy (teamRating team board) $ maximum $
       map (ignorantFixedDepth (n-1) opts team . outcome board) $
           allLegalMoves opts board team
 
-beingSomewhatGreedy :: Weighting -> Weighting -> Weighting
+beingSomewhatGreedy :: Rating -> Rating -> Rating
 beingSomewhatGreedy (WinIn n) _ = WinIn n
 beingSomewhatGreedy (LossIn n) _ = LossIn n
 beingSomewhatGreedy _ (WinIn n) = WinIn (n+1)
 beingSomewhatGreedy _ (LossIn n) = LossIn (n+1)
-beingSomewhatGreedy (Weighting a) (Weighting b) = Weighting $ greedyness*a + (1-greedyness)*b
+beingSomewhatGreedy (Rating a) (Rating b) = Rating $ greedyness*a + (1-greedyness)*b
   where greedyness = 0.1
 
 aiMove :: RuleOptions -> HalmaBoard size -> Team -> Move size
