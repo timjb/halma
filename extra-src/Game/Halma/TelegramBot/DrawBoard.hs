53cda54264e07e89192db1e22ce48070bde2c295
Tim Baumann <tim@timbaumann.info>
2016-06-02 00:41:52 +0200

    draw row numbers next to board

--- extra-src/Game/Halma/TelegramBot/DrawBoard.hs
+++ extra-src/Game/Halma/TelegramBot/DrawBoard.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE LambdaCase #-}
 
 module Game.Halma.TelegramBot.DrawBoard
@@ -8,13 +9,14 @@ import Game.Halma.Board
 import Game.Halma.Board.Draw
 
 import Control.Monad.Catch (MonadMask)
+import Control.Monad.IO.Class (MonadIO (..))
 import Data.Colour.SRGB (sRGB24read)
 import Diagrams.Backend.Cairo (Cairo, renderCairo)
-import Diagrams.Prelude ((#))
+import Diagrams.Prelude ((#), (|||), (*^), (&), (.~))
 import Diagrams.Query (resetValue)
 import Diagrams.Size (dims)
 import Diagrams.TwoD.Types (V2 (..))
-import Control.Monad.IO.Class (MonadIO (..))
+import Numeric (showHex)
 import System.Directory (getTemporaryDirectory)
 import System.IO (hClose)
 import System.IO.Temp (withTempFile)
@@ -28,7 +30,7 @@ withRenderedBoardInPngFile
 withRenderedBoardInPngFile board action =
   withTempPngFilePath $ \path -> do
     let
-      dia = drawBoardForChat board
+      dia = drawBoardForChat board # D.centerXY # D.pad 1.1
       bounds = dims (V2 1000 1000)
     liftIO $ renderCairo path bounds dia
     action path
@@ -41,8 +43,22 @@ withRenderedBoardInPngFile board action =
 
 drawBoardForChat :: HalmaBoard size -> D.Diagram Cairo
 drawBoardForChat board =
-  resetValue (drawBoard' (getGrid board) drawField)
+  let
+    boardDia = resetValue (drawBoard' (getGrid board) drawField)
+  in
+    scale ||| D.strut D.unitX ||| boardDia ||| D.strut D.unitX ||| scale
   where
+    dirY = D.rotateBy (1/6) D.unitX
+    rowDir = dirY & D._x .~ 0
+    radiusInRows = 8 :: Int
+    boardFontStyle x = x # D.fontSize (D.output 22) # D.font "Arial"
+    scale = D.position $ map rowMarker [(-radiusInRows)..radiusInRows]
+      where
+        rowMarker i = (rowPosition i, rowNumber i)
+        rowPosition i = D.p2 $ D.unr2 $ fromIntegral i *^ rowDir
+        rowNumber j =
+          let txt = show (j + radiusInRows)
+          in D.text txt # boardFontStyle # D.fc D.gray
     -- colors from http://clrs.cc/
     botTeamColours :: Team -> D.Colour Double
     botTeamColours =
@@ -53,35 +69,15 @@ drawBoardForChat board =
         South     -> sRGB24read "#FF4136" -- red
         Southeast -> sRGB24read "#111111" -- black
         Southwest -> sRGB24read "#FF851B" -- orange
-    drawPiece
-      :: Piece -> D.Diagram Cairo
-    drawPiece piece =
-      let
-        c = botTeamColours (pieceTeam piece)
-        symbol =
-          case pieceNumber piece of
-            1 -> '1'
-            2 -> '2'
-            3 -> '3'
-            4 -> '4'
-            5 -> '5'
-            6 -> '6'
-            7 -> '7'
-            8 -> '8'
-            9 -> '9'
-            10 -> 'a'
-            11 -> 'b'
-            12 -> 'c'
-            13 -> 'd'
-            14 -> 'e'
-            15 -> 'f'
-            i -> error ("unexpected piece number: " ++ show i)
-        text =
-          D.text [symbol] # D.fc D.white #
-          D.fontSize (D.output 20) # D.font "Arial"
-        circle = D.circle 0.25 # D.fc c # D.lc (D.darken 0.5 c)
-      in
-        text `D.atop` circle
     drawField :: (Int, Int) -> D.Diagram Cairo
     drawField field =
       maybe mempty drawPiece $ lookupHalmaBoard field board
+    drawPiece :: Piece -> D.Diagram Cairo
+    drawPiece piece =
+      let
+        c = botTeamColours (pieceTeam piece)
+        symbol = showHex (pieceNumber piece) ""
+        text = D.text symbol # boardFontStyle # D.fc D.white
+        circle = D.circle 0.3 # D.fc c # D.lc (D.darken 0.5 c)
+      in
+        text `D.atop` circle
